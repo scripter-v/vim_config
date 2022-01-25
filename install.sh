008d1f830a84d8af4fb73c37f168776e78a86133
Vadim Vlasov <vvlasov@pushwoosh.com>
2022-01-25 23:42:25 +0300

    fix linter issues

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@
 set -x
 
 if [ ! -e .installed_packages ]; then
-    cat brew_packages.list | xargs brew install \
+    xargs brew install < brew_packages.list \
         && touch .installed_packages
 fi
 
 if [ ! -e .installed_cask_packages ]; then
-    cat brew_cask_packages.list | xargs brew cask install \
+    xargs brew install --cask < brew_cask_packages.list \
         && touch .installed_cask_packages
 fi
 
 if [ ! -e .installed_pip_modules ]; then
-    cat pip_modules.list | xargs pip install \
+    xargs pip3 install < pip_modules.list \
         && touch .installed_pip_modules
 fi
 
@@ -22,20 +22,20 @@ if [ ! -e .installed_aws_plugin ]; then
         && touch .installed_aws_plugin
 fi
 
-ln -sf $(PWD)/.psqlrc ~/
-ln -sf $(PWD)/.vimrc ~/
-ln -sf $(PWD)/.gitconfig ~/
-ln -sf $(PWD)/.bash_profile ~/
-ln -sf $(PWD)/curl_format.txt ~/
+ln -sf "$(PWD)"/.psqlrc ~/
+ln -sf "$(PWD)"/.vimrc ~/
+ln -sf "$(PWD)"/.gitconfig ~/
+ln -sf "$(PWD)"/.bash_profile ~/
+ln -sf "$(PWD)"/curl_format.txt ~/
 
 if [ ! -e ~/.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
-    mkdir -p  ~/.local/share/nvim/site && \
-    mkdir -p  ~/.config/nvim && \
-    ln -sf ~/.vim/autoload ~/.local/share/nvim/site/autoload && \
-    ln -sf ~/.vim/plugged ~/.config/nvim/plugged && \
-    ln -sf ~/.vimrc ~/.config/nvim/init.vim
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
+        mkdir -p  ~/.local/share/nvim/site && \
+        mkdir -p  ~/.config/nvim && \
+        ln -sf ~/.vim/autoload ~/.local/share/nvim/site/autoload && \
+        ln -sf ~/.vim/plugged ~/.config/nvim/plugged && \
+        ln -sf ~/.vimrc ~/.config/nvim/init.vim
 fi
 
 vim +PlugInstall +qall
